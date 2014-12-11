0c048f0553666c4dedae308591a69582ab828c70
Jeff Conaway <jeff@paceraudio.com>
2014-12-11 13:42:45 -0500

    deleted unused old classes
    Deleted unused AsyncTasks for running Counters and transitions between turns. Counters
    run off of Threads and Handlers now.  Transitions are handled using one AsyncTask

--- app/src/main/java/com/paceraudio/numberreactor/app/ResetNextTurnFadeCounterListener.java
+++
@@ -1,9 +0,0 @@
-package com.paceraudio.numberreactor.app;
-
-/**
- * Created by jeffwconaway on 12/10/14.
- */
-public interface ResetNextTurnFadeCounterListener {
-
-    public void onFadeCounterTurnReset();
-}
