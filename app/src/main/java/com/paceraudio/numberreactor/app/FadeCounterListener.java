96a069750075e1863bc9aefda7a2a301d66d485d
Jeff Conaway <jeff@paceraudio.com>
2014-12-19 09:03:35 -0500

    Deleted an old listener class no longer being used

--- app/src/main/java/com/paceraudio/numberreactor/app/FadeCounterListener.java
+++
@@ -1,9 +0,0 @@
-package com.paceraudio.numberreactor.app;
-
-/**
- * Created by jeffwconaway on 9/21/14.
- */
-public interface FadeCounterListener {
-
-    public void onFadeCountComplete(Double seconds);
-}
